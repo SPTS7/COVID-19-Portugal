cc53749f0ba24b4d2ac62fdf5b509ff282d70a0f
Spts7 <simaopts@gmail.com>
2020-03-24 17:57:58 +0000

    added a pdf printout

--- Previsões_Corona.py
+++ Previsões_Corona.py
@@ -120,6 +120,7 @@ def plot(  # plot all the data
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
     plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=4))
     plt.gcf().autofmt_xdate()
+    plt.savefig("prediction.pdf")
     plt.show()
 
 
